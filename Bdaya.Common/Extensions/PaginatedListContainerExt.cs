27d20130d1ca47c421dd843fc52ebcbe323ada0c
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2022-11-13 12:15:00 +0200

    Adding Extensions

---
+++ Bdaya.Common/Extensions/PaginatedListContainerExt.cs
@@ -0,0 +1,9 @@
+﻿namespace Bdaya.Responses.Linq;
+
+public static class PaginatedListContainerExt
+{
+    public static PaginatedList<TTarget> Select<T, TTarget>(this PaginatedList<T> src, Func<T, TTarget> selector)
+    {
+        return new PaginatedList<TTarget>(pageIndex: src.PageIndex, pageSize: src.PageSize, page: src.Page.Select(selector).ToList(), totalCount: src.TotalCount);
+    }
+}
